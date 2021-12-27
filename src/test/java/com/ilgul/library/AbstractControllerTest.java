9b7464e2622577edc0ff8a787b83ed8a3ca366f8
Illia Hulenko <ihulenk1@educacion.navarra.es>
2021-12-27 01:28:17 +0100

    expired books endpoint and fix tests

--- src/test/java/com/ilgul/library/AbstractControllerTest.java
+++ src/test/java/com/ilgul/library/AbstractControllerTest.java
@@ -1,11 +1,22 @@
 package com.ilgul.library;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ilgul.library.dto.BookDto;
+import com.ilgul.library.dto.ClientDto;
+import com.ilgul.library.repository.BookRepository;
+import com.ilgul.library.repository.ClientRepository;
+import com.ilgul.library.service.TimeService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @Testcontainers
 @SpringBootTest
@@ -16,4 +27,46 @@ public abstract class AbstractControllerTest {
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper objectMapper;
+    @Autowired
+    protected ClientRepository clientRepository;
+    @Autowired
+    protected BookRepository bookRepository;
+
+    @MockBean
+    protected TimeService timeService;
+
+    @SneakyThrows
+    protected ResultActions createClient(ClientDto dto){
+        return mockMvc.perform(post("/client")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)));
+    }
+
+    protected ClientDto mockClient() {
+        ClientDto dto = new ClientDto();
+        dto.setFirstName("Bred");
+        dto.setLastName("Pitt");
+        dto.setEmail("bredpitt@gmail.com");
+        dto.setPhone("+34501234567");
+
+        return dto;
+    }
+
+    @SneakyThrows
+    protected ResultActions createBook(BookDto dto){
+        return mockMvc.perform(post("/book").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)));
+    }
+
+    protected BookDto mockBook() {
+        BookDto testBook = new BookDto();
+        testBook.setName("Test name");
+        testBook.setDescription("Test description");
+        testBook.setAuthor("Test author");
+        testBook.setPublisher("Test publisher");
+        testBook.setYear(2020);
+        testBook.setIsbn("978-617-7866-64-9");
+
+        return testBook;
+    }
 }
