1d9077884e391b0898eed0fc560b62b6e85019f2
Illia Hulenko <ihulenk1@educacion.navarra.es>
2021-12-27 17:23:28 +0100

    fix n+1 issue in expired book in use

--- src/main/java/com/ilgul/library/repository/BookInUseRepository.java
+++ src/main/java/com/ilgul/library/repository/BookInUseRepository.java
@@ -3,6 +3,7 @@ package com.ilgul.library.repository;
 import com.ilgul.library.entity.Book;
 import com.ilgul.library.entity.BookInUse;
 import com.ilgul.library.entity.Client;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.time.LocalDate;
@@ -13,5 +14,6 @@ public interface BookInUseRepository extends PagingAndSortingRepository<BookInUs
 
     Optional<BookInUse> findByClientAndBook(Client client, Book book);
 
+    @EntityGraph(attributePaths = {"client", "book"})
     List<BookInUse> findByInUseFromBefore(LocalDate inUseFromBefore);
 }
